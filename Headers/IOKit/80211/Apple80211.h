7c4177ca24b2db6f1575a647cbce2f8a014adc78
zxystd <1051244836@qq.com>
2020-10-12 09:13:30 +0800

    add more reversed IO80211 constants, update structs

---
+++ Headers/IOKit/80211/Apple80211.h
@@ -0,0 +1,23 @@
+//
+//  Apple80211.h
+//  itlwm
+//
+//  Created by qcwap on 2020/9/4.
+//  Copyright © 2020 钟先耀. All rights reserved.
+//
+
+#ifndef Apple80211_h
+#define Apple80211_h
+
+#include "apple_private_spi.h"
+#include "debug.h"
+#include "IO80211WorkLoop.h"
+#include "IO80211Controller.h"
+#include "IO80211Interface.h"
+#include "IO80211VirtualInterface.h"
+#include "IO80211P2PInterface.h"
+#if __IO80211_TARGET >= __MAC_10_15
+#include "IO80211SkywalkInterface.h"
+#endif
+
+#endif /* Apple80211_h */
