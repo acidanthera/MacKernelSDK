2c3ab5adeff01ca3a16222ef17bb9497089e9201
Charlie Jiang <CharlieJiangXXX@users.noreply.github.com>
2021-11-06 20:16:34 -0400

    Remove duplicate version definitions from AvailabilityVersions.h

--- Headers/AvailabilityVersions.h
+++ Headers/AvailabilityVersions.h
@@ -180,49 +180,6 @@
 #define __WATCHOS_7_6     70600
 #define __WATCHOS_8_0     80000
 
-/*
- * Set up standard Mac OS X versions
- */
-
-#if (!defined(_POSIX_C_SOURCE) && !defined(_XOPEN_SOURCE)) || defined(_DARWIN_C_SOURCE)
-
-#define MAC_OS_X_VERSION_10_0         1000
-#define MAC_OS_X_VERSION_10_1         1010
-#define MAC_OS_X_VERSION_10_2         1020
-#define MAC_OS_X_VERSION_10_3         1030
-#define MAC_OS_X_VERSION_10_4         1040
-#define MAC_OS_X_VERSION_10_5         1050
-#define MAC_OS_X_VERSION_10_6         1060
-#define MAC_OS_X_VERSION_10_7         1070
-#define MAC_OS_X_VERSION_10_8         1080
-#define MAC_OS_X_VERSION_10_9         1090
-#define MAC_OS_X_VERSION_10_10      101000
-#define MAC_OS_X_VERSION_10_10_2    101002
-#define MAC_OS_X_VERSION_10_10_3    101003
-#define MAC_OS_X_VERSION_10_11      101100
-#define MAC_OS_X_VERSION_10_11_2    101102
-#define MAC_OS_X_VERSION_10_11_3    101103
-#define MAC_OS_X_VERSION_10_11_4    101104
-#define MAC_OS_X_VERSION_10_12      101200
-#define MAC_OS_X_VERSION_10_12_1    101201
-#define MAC_OS_X_VERSION_10_12_2    101202
-#define MAC_OS_X_VERSION_10_12_4    101204
-#define MAC_OS_X_VERSION_10_13      101300
-#define MAC_OS_X_VERSION_10_13_1    101301
-#define MAC_OS_X_VERSION_10_13_2    101302
-#define MAC_OS_X_VERSION_10_13_4    101304
-#define MAC_OS_X_VERSION_10_14      101400
-#define MAC_OS_X_VERSION_10_14_1    101401
-#define MAC_OS_X_VERSION_10_14_4    101404
-#define MAC_OS_X_VERSION_10_14_6    101406
-#define MAC_OS_X_VERSION_10_15      101500
-#define MAC_OS_X_VERSION_10_15_1    101501
-#define MAC_OS_X_VERSION_10_16      101600
-#define MAC_OS_VERSION_11_0         110000
-#define MAC_OS_VERSION_12_0         120000
-
-#endif /* #if (!defined(_POSIX_C_SOURCE) && !defined(_XOPEN_SOURCE)) || defined(_DARWIN_C_SOURCE) */
-
 #define __DRIVERKIT_19_0 190000
 #define __DRIVERKIT_20_0 200000
 #define __DRIVERKIT_21_0 210000
