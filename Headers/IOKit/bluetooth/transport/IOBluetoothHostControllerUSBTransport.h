0f4f4d404a226ed85d3e170696f76fad0151c5e8
Charlie Jiang <CharlieJiangXXX@users.noreply.github.com>
2021-11-16 13:53:07 -0500

    Compat USB transport with 10.14 and 10.13.

--- Headers/IOKit/bluetooth/transport/IOBluetoothHostControllerUSBTransport.h
+++ Headers/IOKit/bluetooth/transport/IOBluetoothHostControllerUSBTransport.h
@@ -152,7 +152,10 @@ public:
 
     virtual IOReturn ProcessG3StandByWake();
     virtual IOReturn ReConfigure();
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
     virtual bool     NeedToTurnOnUSBDebug();
+#endif
 
     virtual IOReturn HardReset() APPLE_KEXT_OVERRIDE;
 
