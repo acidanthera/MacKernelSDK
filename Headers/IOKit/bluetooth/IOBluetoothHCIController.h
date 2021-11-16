f828837e5e5ef904414f2111fd7cd05e5840aa80
Charlie Jiang <CharlieJiangXXX@users.noreply.github.com>
2021-11-15 20:56:16 -0500

    Compat IOBluetoothHCIController with macOS 10.13 and 10.14.

--- Headers/IOKit/bluetooth/IOBluetoothHCIController.h
+++ Headers/IOKit/bluetooth/IOBluetoothHCIController.h
@@ -49,6 +49,7 @@ class IOWorkQueue;
 class IODisplayWrangler;
 class IOBluetoothACPIMethods;
 class IOBluetoothPacketLogger;
+class IOBluetoothHCIPacketLogUserClient;
 class IOBluetoothHostController;
 class IOBluetoothHostControllerTransport;
 class IOBluetoothHostControllerUSBTransport;
@@ -74,6 +75,15 @@ struct HCIEventNotificationListener
     void *      refCon;
     UInt64      unknown; // All set to 1
 };
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_15
+struct BluetoothPacketLogData
+{
+    UInt32 creationTime;
+    UInt32 packetSize;
+    UInt8  packetType;
+};
+#endif
 
 extern UInt32 IOBluetoothRingBufferRead(UInt32 *, UInt8 *, UInt32, UInt32);
 extern UInt32 IOBluetoothRingBufferReadAtOffset(UInt32 *, void *, UInt32, UInt64, UInt32);
@@ -105,6 +115,9 @@ public:
     virtual IOReturn terminateWL(IOOptionBits options = 0);
 
     virtual IOReturn newUserClient(task_t owningTask, void * securityID, UInt32 type, LIBKERN_RETURNS_RETAINED IOUserClient ** handler) APPLE_KEXT_OVERRIDE;
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_15
+    virtual IOReturn AddPacketLogUserClient(IOBluetoothHCIPacketLogUserClient * client);
+#endif
     virtual IOReturn AddCommandUserClient(void * userClient);
     virtual void     DetachUserClients();
 
@@ -116,9 +129,13 @@ public:
     virtual void terminateServiceNubs();
     virtual bool terminateServiceNubsComplete();
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     static void USBHardResetEntry(OSObject * owner);
     static bool PacketLoggerPublishNotificationHandler(void *, void *, IOService *, IONotifier *);
     static bool HandlePacketLoggerPublishNotification(void *, IOService *);
+#else
+    static void HardResetEntry(OSObject * owner);
+#endif
     static bool DisplayManagerPublishNotificationHandler(void *, void *, IOService *, IONotifier *);
     static bool HandleDisplayManagerPublishNotification(void *, IOService *);
     static bool ExternalDisplayTerminateNotificationHandler(void *, void *, IOService *, IONotifier *);
@@ -139,10 +156,21 @@ public:
 
     virtual IOWorkLoop *    getWorkLoop() const APPLE_KEXT_OVERRIDE;
     virtual IOCommandGate * getCommandGate() const;
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     virtual IOWorkLoop *    getUSBHardResetWorkLoop() const;
     virtual IOCommandGate * getUSBHardResetCommandGate() const;
+#else
+    virtual bool PacketLogFillBufferedData(IOBluetoothHCIPacketLogUserClient * client);
+    virtual void PacketLogSetBufferEmptiedFlag();
+    virtual void PacketLogClientClosed(IOBluetoothHCIPacketLogUserClient * client);
+#endif
 
     virtual void LogPacket(UInt8 packetType, void * packetData, size_t packetSize);
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_15
+    static IOReturn LogPacketAction(OSObject * owner, void * arg0, void * arg1, void * arg2, void * arg3, void * arg4, void * arg5);
+    virtual void LogPacketData(UInt8 packetType, void * packetData, size_t packetSize);
+#endif
 
     virtual bool     shouldOverrideExistingController(IOBluetoothHCIController *, BluetoothHardwareListType *);
     virtual IOReturn SwitchToSelectedHostController(UInt32 locationID);
@@ -219,11 +247,21 @@ public:
     virtual bool SetBluetoothFamilyProperty(const char * aKey, void * bytes, unsigned int length);
 
     virtual bool     UpdateActiveControllerProperties(char *);
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
     virtual UInt32   GetNextBluetoothObjectID();
     virtual IOReturn FreeBluetoothObjectID(UInt32);
     virtual void     CheckACPIMethodsAvailabilities();
+#else
+    virtual void     CheckSpecialGPIO();
+    virtual IOReturn SetBTRS();
+    virtual IOReturn SetBTPU();
+    virtual IOReturn SetBTPD();
+    virtual IOReturn SetBTRB(bool);
+    virtual IOReturn SetBTLP(bool);
+#endif
     virtual IOReturn CallRegisterService();
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     virtual bool  ReachHardResetLimit(IOBluetoothHostController *);
     virtual void  IncrementHardResetCounter(IOBluetoothHostController *);
     virtual void  ResetHardResetCounter(IOBluetoothHostController *);
@@ -232,10 +270,19 @@ public:
     virtual IOReturn USBHardReset();
     static IOReturn  USBHardResetAction(OSObject * owner, void * arg0, void * arg1, void * arg2, void * arg3);
     virtual void     USBHardResetWL();
+#else
+    virtual IOReturn PerformHardwareReset();
+    static IOReturn  HardwareResetAction(OSObject * owner, void * arg0, void * arg1, void * arg2, void * arg3);
+    virtual void     HardwareResetWL();
+#endif
     virtual void     ReleaseAllHardResetObjects();
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     virtual bool     FindBroadcomUSBHostDevice();
     virtual bool     USBBluetoothModuleWithROMBootCapability();
     virtual IOReturn RecoverX238EModule(BluetoothHardwareListType *);
+#else
+    virtual bool     FindBroadcomUSBHostController();
+#endif
 
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_11_0
     virtual void     RecoveryTimeOutHandler();
@@ -308,16 +355,32 @@ public:
 protected:
     IOWorkLoop *    mWorkLoop;                // 136
     IOCommandGate * mCommandGate;             // 144
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     IOWorkLoop *    mUSBHardResetWorkLoop;    // 152
     IOCommandGate * mUSBHardResetCommandGate; // 160
+#else
+    OSSet * mPacketLoggerUserClientSet;       /// 152
+#endif
+    OSSet * mCommandUserClientSet;            // 168
 
-    OSSet *                   mUserClientSet;                    // 168
-    IOBluetoothPacketLogger * mPacketLogger;                     // 176
-    IONotifier *              mPacketLoggerPublishNotifier;      // 184
-    IODisplayWrangler *       mDisplayManager;                   // 192
-    IONotifier *              mDisplayManagerPublishNotifier;    // 200
-    IONotifier *              mExternalDisplayPublishNotifier;   // 208
-    IONotifier *              mExternalDisplayTerminateNotifier; // 216
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
+    IOBluetoothPacketLogger * mPacketLogger;                // 176
+    IONotifier *              mPacketLoggerPublishNotifier; // 184
+#else
+    BLueotothPacketLogData * mPacketLogBuffer;             /// 168
+    UInt32                   mPacketLogBufferSize;         /// 176
+    OSArray *                mLogPackets;                  /// 184
+    UInt32                   mMaxNumberOfPreStoredPackets; /// 192
+    UInt32                   mLogPacketIndex;              /// 196
+    UInt32                   mPacketLogFillDataIndex;      /// 200
+    bool                     mNewPacketLogUserClient;      /// 204
+    bool                     mPacketLogBufferEmptied;      /// 205
+#endif
+
+    IODisplayWrangler * mDisplayManager;                   // 192
+    IONotifier *        mDisplayManagerPublishNotifier;    // 200
+    IONotifier *        mExternalDisplayPublishNotifier;   // 208
+    IONotifier *        mExternalDisplayTerminateNotifier; // 216
 
     bool                 mSearchForTransportEventTimerHasTimeout; // 224
     IOTimerEventSource * mSearchForTransportEventTimer;           // 232
@@ -330,6 +393,9 @@ protected:
 
     IOWorkQueue * mFamilyWorkQueue;             // 272
     IOWorkQueue * mExternalControllerWorkQueue; // 280
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_15
+    IOWorkQueue * mLogPacketWorkQueue;          /// 304
+#endif
 
     BluetoothHardwareListType * mBluetoothHardwareListHead;         // 288
     BluetoothHardwareListType * mBluetoothHardwareListTail;         // 296
@@ -372,35 +438,54 @@ protected:
 
     os_log_t mInternalOSLogObject;   // 424
     bool     unknown;                // 432, has to do with power state in transports
-    bool     mBootFromROM;           // 433
-    bool     mBTGPIOResult;          // 434, BTRB result
-    bool     mRegisterServiceCalled; // 435
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_14
+    IOACPIPlatformDevice * mACPIDevice;  /// 464
+    bool                   SupportBTRS;  /// 472
+    bool                   SupportBTRB;  /// 473
+    bool                   mSupportBTPU; /// 474
+    bool                   mSupportBTPD; /// 475
+    bool                   mSupportBTLP; /// 476
+#endif
+
+    bool mBootFromROM;           // 433
+    bool mBTGPIOResult;          // 434, BTRB result
+    bool mRegisterServiceCalled; // 435
 
     thread_call_t mHardResetThreadCall;                    // 440
     UInt32        mUSBHardResetWLCallTime;                 // 448
+
     bool          mTestHCICommandTimeoutUSBHardResetWL;    // 452
     bool          mTestHCICommandTimeoutHardReset;         // 453
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     bool          unknown1;                                // 454, see DispatchHardwareResetTest
     bool          mTestNoHardResetWhenSleepCommandTimeout; // 455
     bool          mTestHCICommandTimeoutWhenWake;          // 456
     bool          unknown2;                                // 457
-
+#endif
     UInt8                                   mNumHardResetRetries;    // 458
     IOBluetoothHostControllerUSBTransport * mHardResetUSBTransport;  // 464
     IOUSBHostDevice *                       mHardResetUSBHostDevice; // 472
     IOUSBHostDevice *                       mHardResetUSBHub;        // 480
+
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
     IOBluetoothACPIMethods *                mACPIMethods;            // 488
+#endif
 
 #if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_11_0
     IOTimerEventSource * mRecoveryTimer;           // 496
     bool                 mRecoveryTimerHasTimeout; // 504
 #endif
 
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_14
     UInt16   mCurrentBluetoothObjectID;     // 506
     bool     mBluetoothObjects[0xFFFF];     // 508
     IOLock * mBluetoothObjectsLock;         // 66048
     UInt8    mNumHCIEventListeners;         // 66056
+#endif
+#if __MAC_OS_X_VERSION_MIN_REQUIRED >= __MAC_10_15
     bool     mNotTriedToRecoverX238EModule; // 66057
+#endif
 
     struct ExpansionData
     {
