e18b98441a99f149d0448769a8fa43c92f7a1fc6
Charlie Jiang <CharlieJiangXXX@users.noreply.github.com>
2021-11-16 14:16:29 -0500

    My bad...revert!

--- Headers/IOKit/bluetooth/transport/IOBluetoothHostControllerTransport.h
+++ Headers/IOKit/bluetooth/transport/IOBluetoothHostControllerTransport.h
@@ -231,7 +231,7 @@ public:
     virtual bool     WaitForSystemReadyForSleep(char *);
     virtual IOReturn StartBluetoothSleepTimer();
     virtual void     CancelBluetoothSleepTimer();
-#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_14
+#if __MAC_OS_X_VERSION_MIN_REQUIRED < __MAC_10_15
     virtual bool     StopAllReads();
 #endif
     virtual os_log_t CreateOSLogObject();
